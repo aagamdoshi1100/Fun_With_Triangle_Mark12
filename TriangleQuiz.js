82fee6ed86be203addcd11afe5c1ad1507b3868c
Aagam Doshi <aagamdoshi1100@gmail.com>
2022-10-23 15:39:08 +0530

    Added quiz questions

--- TriangleQuiz.js
+++ TriangleQuiz.js
@@ -2,7 +2,7 @@ var DataOfForm = document.querySelector("#Quiz-form");
 var ChkBtn = document.querySelector("#submit");
 var Output =document.querySelector("#out");
 
-var CorrectAnswer = ['90','right angled'];
+var CorrectAnswer = ['90','right angled',"3 sides, 3 angles, and 3 vertices","180","sum of side lengths of the triangle"];
 
 
 ChkBtn.addEventListener("click", ScoreBoard);
